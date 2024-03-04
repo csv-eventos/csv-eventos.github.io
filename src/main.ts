3cb2639def684ca283bc9e539f164f188f543ff1
Nelson Alexandre Mutane <nelson.mutane@uzambeze.ac.mz>
2024-03-04 21:46:38 +0200

    #fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ViteSSG } from 'vite-ssg/single-page'
-import App from './App.vue'
 import './style.css'
+import App from './App.vue'
+
 // `export const createApp` is required instead of the original `createApp(App).mount('#app')`
 export const createApp = ViteSSG(App)
